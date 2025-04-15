02b8e451af3968e7f49ed67ae3777e62131c2769
maoyuanshou <126571815+maoyuanshou@users.noreply.github.com>
2025-04-15 22:50:21 +0800

    Delete my-vue-project-master directory

--- my-vue-project-master/express/config.js
+++
@@ -1,7 +0,0 @@
-//这是一个全局的配置文件
-module.exports = {
-    //加密和解密Token的密钥
-    jwtSecretKey: 'itheima No1.Gaoyuan',
-    //Token的有效期
-    expiresIn: '10h'
-}
--- my-vue-project-master/src/common/js/dateFormat.js
+++
@@ -1,8 +0,0 @@
-export function formateDate(str){
-    let date = new Date();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-    month > 10 ? month : "0" + month;
-    day > 10 ? day: "0" + day;
-    return month + '-' + day + " " + str;
-}
